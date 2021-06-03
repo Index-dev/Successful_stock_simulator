66f1cf9107d5f4358baf91f7186168a5aaf9a1cc
EumYoungHyun <opwer032@naver.com>
2021-06-03 21:07:37 +0900

    feat: Add pipenv

--- server/crawling/stock.py
+++ server/crawling/stock.py
@@ -1,2 +1,11 @@
+import pandas as pd
+
 def getKOSDAQStockInfo():
-    url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=%s%kosdaqMkt' 
+    url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=%s' % 'kosdaqMkt' 
+    df = pd.read_html(url, header=0)[0]
+
+    return df
+
+if __name__ == '__main__':
+    result_df = getKOSDAQStockInfo()
+    print(result_df)
